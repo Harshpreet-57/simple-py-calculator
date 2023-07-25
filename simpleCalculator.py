2ca6bc027a32f6100fa3afbb8ea924d58f69785e
Quadrant <102208083+Harshpreet-57@users.noreply.github.com>
2023-07-25 09:18:33 +0530

    Add files via upload

---
+++ simpleCalculator.py
@@ -0,0 +1,19 @@
+# ⁡⁣⁢⁣𝗦𝗶𝗺𝗽𝗹𝗲 𝗰𝗮𝗹𝗰𝘂𝗹𝗮𝘁𝗼𝗿⁡
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+operator = str(input("Enter an operator:"))
+
+if operator == "+":
+    print(a+b)
+elif operator == "-":
+    print(a-b)
+elif operator == "*":
+    print(a*b)
+elif operator == "/":
+    print(a/b)
+elif operator == "**":
+    print(a**b)
+else:
+    print("Invalid operator")
+ 
